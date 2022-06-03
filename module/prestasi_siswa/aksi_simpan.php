2928e4ebb0e2c14e738efc6a7ab156c734352e41
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-06-03 22:31:32 +0700

    tes

--- module/prestasi_siswa/aksi_simpan.php
+++ module/prestasi_siswa/aksi_simpan.php
@@ -25,7 +25,7 @@
     $redirectFailed = "main.php?module=tambah_prestasi_siswa";
 
     if ($querySimpan) {
-        echo "<script> alert('Data Prestasi Siswa Berhasil Masuk'); window.location = '$base_url'+'$redirectSucces';</script>";
+        echo "<script> alert('Data Prestasi Siswa Berhasil Masuk'); window.location = '$base_url'+'$redirectSuccess';</script>";
     }
     else {
         echo "<script> alert('Data Pelanggaran Siswa Gagal Dimasukkan'); window.location = '$base_url'+'$redirectFailed';</script>";
